fe8145f226ccf4e681f8277b77d97564b8e17e29
Isaac <isaac@Isaacs-MacBook-Pro.local>
2019-07-29 18:03:44 -0500

    Signup and Login Components Added

--- src/index.js
+++ src/index.js
@@ -4,28 +4,40 @@ import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-// 1
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-// 2
+import { BrowserRouter } from "react-router-dom";
+import { setContext } from "apollo-link-context";
+import { AUTH_TOKEN } from "./constants";
+
 const httpLink = new createHttpLink({
   uri: "http://localhost:4000"
 });
 
-// 3
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
-// 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
